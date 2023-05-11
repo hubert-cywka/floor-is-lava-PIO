dbcb2a8ee7cdc350aae2f0e43c9af4a0ce40bc45
Hubert Cywka <hcywka@gmail.com>
2023-05-11 11:48:47 +0200

    PIO-6: SpringBoot + H2 setup

---
+++ Back-end/floor-is-lava/src/main/java/com/pio/floorislava/FloorIsLavaApplication.java
@@ -0,0 +1,11 @@
+package com.pio.floorislava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorIsLavaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FloorIsLavaApplication.class, args);
+	}
+}
