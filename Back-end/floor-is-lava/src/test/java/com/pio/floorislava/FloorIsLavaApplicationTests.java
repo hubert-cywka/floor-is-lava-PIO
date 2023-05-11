811781ea4ffe83c651e5929676f69c016132497c
Hubert Cywka <hcywka@gmail.com>
2023-05-11 11:48:47 +0200

    PIO-6: SpringBoot + H2 setup

---
+++ Back-end/floor-is-lava/src/test/java/com/pio/floorislava/FloorIsLavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pio.floorislava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorIsLavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
