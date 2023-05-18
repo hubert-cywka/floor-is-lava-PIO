e87ffec4c5f4122b9711b7600ee1092ed53eb711
Hubert Cywka <hcywka@gmail.com>
2023-05-18 11:44:31 +0200

    PIO-8.1: Updated initial screen layout

--- floor-is-lava-front/src/main/java/com/pio/floorislavafront/FloorIsLavaApp.java
+++ floor-is-lava-front/src/main/java/com/pio/floorislavafront/FloorIsLavaApp.java
@@ -2,7 +2,9 @@ package com.pio.floorislavafront;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,10 @@ import java.io.IOException;
 public class FloorIsLavaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
         FXMLLoader fxmlLoader = new FXMLLoader(FloorIsLavaApp.class.getResource("initial-screen-scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), bounds.getWidth(), bounds.getHeight() - 20);
         stage.setTitle("Floor is Lava!");
         stage.setScene(scene);
         stage.show();
