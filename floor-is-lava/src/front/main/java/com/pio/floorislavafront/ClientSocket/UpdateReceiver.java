d559082113ded0f3b146802c4b9a5eba2777e467
Nerekk <arkadiusz.salaj.02@gmail.com>
2023-05-24 23:56:32 +0200

    Implementing map display and refresh

--- floor-is-lava/src/front/main/java/com/pio/floorislavafront/ClientSocket/UpdateReceiver.java
+++ floor-is-lava/src/front/main/java/com/pio/floorislavafront/ClientSocket/UpdateReceiver.java
@@ -1,12 +1,14 @@
 package front.main.java.com.pio.floorislavafront.ClientSocket;
 
 import common.Packet;
+import javafx.application.Platform;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.Serializable;
 
-public class UpdateReceiver implements Runnable{
+import static front.main.java.com.pio.floorislavafront.DisplayUtils.DisplayHandler.mapHandler;
+
+public class UpdateReceiver implements Runnable {
 
     private final ObjectInputStream objectInputStream;
 
@@ -21,10 +23,17 @@ public class UpdateReceiver implements Runnable{
 
             while (true) {
                 Packet packet = (Packet) objectInputStream.readObject();
+
+                // Printing map to console
+//                packet.printMap();
+
+                // Refreshing map
+                Platform.runLater(() -> mapHandler(packet.map));
+
                 System.out.println("(Update from server) " + packet.message);
             }
 
-        }catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
