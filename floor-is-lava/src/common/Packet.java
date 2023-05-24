5d376b62f0848fbb90e8f2e8732eaaa7df4e6323
Nerekk <arkadiusz.salaj.02@gmail.com>
2023-05-24 23:52:17 +0200

    Preparing packet for sending map through sockets

--- floor-is-lava/src/common/Packet.java
+++ floor-is-lava/src/common/Packet.java
@@ -3,7 +3,19 @@ import java.io.Serializable;
 
 public class Packet implements Serializable {
     public String message;
-    public Packet(String message){
+    public char[][] map;
+    public Packet(String message, char[][] map){
         this.message = message;
+        this.map = map;
+    }
+
+    //TESTING METHOD
+    public void printMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
