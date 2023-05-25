8f0c6866a981689dd0e11bd9e3e78c677c19a92d
zurson <matu.jurek@gmail.com>
2023-05-25 13:00:53 +0200

    Font refactoring

---
+++ floor-is-lava/src/front/main/java/com/pio/floorislavafront/DisplayUtils/FieldType.java
@@ -0,0 +1,15 @@
+package front.main.java.com.pio.floorislavafront.DisplayUtils;
+
+public enum FieldType {
+
+    PLAYER_0,
+    PLAYER_1,
+    PLAYER_2,
+    PLAYER_3,
+    WALL,
+    BORDER,
+    SAFE_ZONE,
+    FLOOR,
+    LAVA
+
+}
