92131483226709cdee2999f1af1df59d48426753
zurson <matu.jurek@gmail.com>
2023-05-27 17:12:30 +0200

    Addition of timer field + getter to Packet class

--- floor-is-lava/src/common/Packet.java
+++ floor-is-lava/src/common/Packet.java
@@ -4,13 +4,19 @@ import java.io.Serializable;
 
 public class Packet implements Serializable {
     private final byte[] map;
+    private final int timer;
 
 
-    public Packet(byte[] map) {
+    public Packet(byte[] map, int timer) {
         this.map = map;
+        this.timer = timer;
     }
 
     public byte[] getMap() {
         return map;
+    }
+
+    public int getTimer() {
+        return timer;
     }
 }
