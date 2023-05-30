c3a56391f543f9a229020c29b8bb5cea6c119671
zurson <matu.jurek@gmail.com>
2023-05-30 12:36:07 +0200

    Created field in Packet class that contains PlayerData

--- floor-is-lava/src/common/Packet.java
+++ floor-is-lava/src/common/Packet.java
@@ -1,15 +1,29 @@
 package common;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+
+import static common.GlobalSettings.MAX_PLAYERS;
 
 public class Packet implements Serializable {
     private final byte[] map;
     private final int timer;
+    private final ArrayList<PlayerData> playerData = new ArrayList<>(MAX_PLAYERS);
 
 
     public Packet(byte[] map, int timer) {
         this.map = map;
         this.timer = timer;
+    }
+
+
+    public void addPlayerData(PlayerData newData){
+
+        if (playerData.size() >= MAX_PLAYERS)
+            return;
+
+        playerData.add(newData);
+
     }
 
     public byte[] getMap() {
