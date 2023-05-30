d9620594e8bbf39195f0fe7b8f43093f721e6c15
zurson <matu.jurek@gmail.com>
2023-05-30 12:35:36 +0200

    Created new PlayerData class

---
+++ floor-is-lava/src/common/PlayerData.java
@@ -0,0 +1,38 @@
+package common;
+
+public class PlayerData {
+
+    private String nickname;
+    private boolean isAlive;
+    private boolean isConnected;
+
+    public PlayerData(String nickname, boolean isAlive, boolean isConnected) {
+        this.nickname = nickname;
+        this.isAlive = isAlive;
+        this.isConnected = isConnected;
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
+    }
+}
