7bbe89beb7af2cb495e030d6e598d683cd465361
zurson <matu.jurek@gmail.com>
2023-06-05 19:13:01 +0200

    New exception catch

--- floor-is-lava/src/back/GameMap.java
+++ floor-is-lava/src/back/GameMap.java
@@ -181,6 +181,10 @@ public class GameMap implements Serializable {
     private void actionWhenSafezoneOnPlayer(int row, int col, FieldType tile){
         if (PLAYER_FIELDS.contains(map[row][col]) && (tile == FieldType.SAFE_ZONE)){
             Player player = game.findPlayerByFiledType(map[row][col]);
+
+            if (player == null)
+                return;
+
             player.setLastStandingField(FieldType.SAFE_ZONE);
         }
     }
